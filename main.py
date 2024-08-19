87a8eeee75299b74765affec507275ab551e9eab
akuntes126 <akunntteess02@gmail.com>
2024-08-19 11:16:50 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Arahkan ke direktori root atau direktori lain tempat file `index.html` berada
+    return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
